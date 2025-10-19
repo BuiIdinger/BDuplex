c4f383f932442c1eb02f207e68b4d02d400ae9fa
Shayden seymour <shaydenseymour@gmail.com>
2025-10-19 15:42:16 +1300

    Commit starter files

---
+++ ExecMain.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "src/BDuplex.h"
+
+int main() {
+  BDuplex::hi();
+  return EXIT_SUCCESS;
+}
---
+++ src/BDuplex.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace BDuplex {
+  inline void hi() {
+    std::cout << "Hello world\n";
+  }
+}
